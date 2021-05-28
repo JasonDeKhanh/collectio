1aa465462d8fb16b8b8dc8cd02b2ee97d5d25c1d
br234 <brendahangjh@gmail.com>
2021-05-28 16:47:18 +0800

    Added Login page and My Albums page

---
+++ collectio/src/pages/MyAlbumsPage.js
@@ -0,0 +1,15 @@
+import AppBar1 from "../components/AppBar1";
+
+function MyAlbumsPage() {
+
+  return (
+    <>
+      <AppBar1 />
+      <main>
+
+      </main>
+    </>
+  );
+}
+
+export default MyAlbumsPage;
