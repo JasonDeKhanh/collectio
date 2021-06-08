76e263cfa437013693017130dc478717a66b424d
Jason <giakhanh.lehoang@gmail.com>
2021-06-08 14:56:30 +0800

    start create new album menu

--- src/components/CreateNewAlbumPopup/CreateNewAlbumPopup.js
+++ src/components/CreateNewAlbumPopup/CreateNewAlbumPopup.js
@@ -11,9 +11,32 @@ function CreateNewAlbumPopup(props) {
   return (
     <div>
       <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-        <h1>Hello bleh bleh bleh</h1>
-        Test test walah
+        <h1>Create New Album</h1>
+
         <br></br>
+        <form>
+          <div>
+            <label htmlFor="album-name">Album Name:</label>
+            <br />
+            <input type="text" name="name" id="album-name" />
+          </div>
+          <br></br>
+          Orientation:
+          <div>
+            <input
+              type="radio"
+              name="orientation-portrait"
+              id="album-orientation-portrait"
+            />
+            <label htmlFor="album-orientation-portrait">Portrait</label>
+            <input
+              type="radio"
+              name="orientation-landscape"
+              id="album-orientation-landscape"
+            />
+            <label htmlFor="album-orientation-landscape">Landscape</label>
+          </div>
+        </form>
         <br></br>
         <button> Create new album </button>
       </Popup>
