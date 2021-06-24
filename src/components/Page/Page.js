47ff3cace757e6976029b2b169317910d771cdea
Jason <giakhanh.lehoang@gmail.com>
2021-06-24 21:26:58 +0800

    create Page js

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -7,6 +7,17 @@ function Page(props) {
   return (
     <div>
       <h1>hello page</h1>
+      {/* return some Paper/Background Item here I guess, maybe something like
+      <AlbumBackground props={props}/>
+      the props here could be the attributes required for the background, eg. color, patterns blabla whatever
+
+      const items = idk retrieve the array from firesetore
+
+      items.map((item) => {
+        <AlbumItem item={item} /> smthing like this
+      })
+      
+        */}
     </div>
   );
 }
